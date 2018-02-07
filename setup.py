2ddcfda0a318786756b48dbeaef60bdf2cb21ab9
KOLANICH <kolan_n@mail.ru>
2018-02-07 20:32:44 +0300

    Actually used kaitaiStructCompile

--- setup.py
+++ setup.py
@@ -6,9 +6,27 @@ from setuptools import setup
 from setuptools.config import read_configuration
 
 from pathlib import Path
+thisDir=Path(__file__).parent
+formatsPath=thisDir / "kaitai_struct_formats"
 
-cfg = read_configuration(str(Path(__file__).parent / 'setup.cfg'))
+cfg = read_configuration(str((thisDir / 'setup.cfg').absolute()))
 #print(cfg)
 cfg["options"].update(cfg["metadata"])
 cfg=cfg["options"]
+
+cfg["kaitai"]={
+    "formatsRepo": {
+        "localPath" : str(formatsPath),
+        "update": True
+    },
+    "formats":{
+        "heroes_of_might_and_magic_agg.py": {"path": "game/heroes_of_might_and_magic_agg.ksy"},
+        "quake_pak.py": {"path": "game/quake_pak.ksy"},
+        "iso9660.py": {"path": "filesystem/iso9660.ksy"},
+        "rar.py": {"path": "archive/rar.ksy"},
+    },
+    "outputDir": thisDir / "kaitaifs" / "parser",
+    "inputDir": formatsPath
+}
+
 setup(**cfg)
